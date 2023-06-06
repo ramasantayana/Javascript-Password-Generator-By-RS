b18cc62c1c863672437e6603a1b787da4e1e2d56
ramasantayana <rama.santayana@uconn.edu>
2023-06-06 13:26:14 -0400

    Edited lines 4 to 7 on JS code

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let characterLength = 8;
 let choiceArray = [];
 
-let specialCharacterArray = ['!','@','#','$','%','^','&','*','(',')','[',']','{','}','|','?','/','<','>'];
-let lowerCaseArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-let upperCaseArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-let numberArray = ['1','2','3','4','5','6','7','8','9','0'];
+let specialCharacterArray = ["!","@","#","$","%","^","&","*","(",")","[","]","{","}","|","?","/","<",">"];
+let lowerCaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+let upperCaseArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+let numberArray = ["1","2","3","4","5","6","7","8","9","0"];
 
 var generateBtn = document.querySelector("#generate");
 
